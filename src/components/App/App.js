f607f419f0bcd2a23a3cb79283ce471a019a7524
MaxDaniLOVE <mahseeen@gmail.com>
2020-03-11 12:52:52 +0300

    init

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+
+export default class App extends Component {
+  state = {
+    data: [],
+  }
+
+  componentDidMount() {
+    //
+  }
+
+  render() {
+    return (
+      <div className="container">
+        canvas
+      </div>
+    );
+  }
+}
